7e99fa18f6a285082bbfbbd43c46815013b60469
mon0mon <mon0mon@naver.com>
2023-01-21 19:05:01 +0900

    2023-01-21

---
+++ Examples/Basic/06-simple_component/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <ol>
+    <TodoItem v-for="item in groceryList" :todo="item" :key="item.id"></TodoItem>
+  </ol>
+</template>
+
+<script setup>
+
+import TodoItem from "@/components/TodoItem.vue";
+import {ref} from 'vue';
+
+const groceryList = ref([
+  {id: 0, text: 'Vegetables'},
+  {id: 1, text: 'Cheese'},
+  {id: 2, text: 'Whatever else humans are supposed to eat'}
+])
+
+</script>
+
+<style>
+</style>
---
+++ Examples/Basic/06-simple_component/src/components/TodoItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <li>{{todo.text}}</li>
+</template>
+
+<script setup>
+
+// eslint-disable-next-line no-unused-vars,no-undef
+const props = defineProps({
+  todo:Object
+})
+
+</script>
+
+<style scoped>
+
+</style>
